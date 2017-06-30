4fa5a2b0625424eecc0f97b414655f8c90eb88bc
pessonnier <loic.pessonnier@gmail.com>
2017-06-30 23:48:37 +0200

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -1,8 +1,8 @@
-import RPi.GPIO as GPIO
 import time
 import subprocess
 
 import identification as id
+import pied
 import upload
 import play
 import bluetooth as bt
@@ -30,13 +30,6 @@ MOUVEMENT_OFF=103
 TOUCH=4
 TOUCH_OFF=104
 
-# les GPIO
-BTTROUGE_GPIO=23
-BTTVERT_GPIO=24
-MOUVEMENT_GPIO=17
-HORIZONTAL_GPIO=18
-VERTICAL_GPIO=27
-
 def attente(trs):
   ev=[]
   while ev == []:
@@ -62,16 +55,7 @@ def attente(trs):
 def init():
   print('init')
   # les périfériques
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(BTTROUGE_OFF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(BTTVERT_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(MOUVEMENT_GPIO, GPIO.IN)
-  GPIO.setup(HORIZONTAL_GPIO, GPIO.OUT)
-  GPIO.setup(VERTICAL_GPIO, GPIO.OUT)
-  h = GPIO.PWM(HORIZONTAL_GPIO, 50) 
-  v = GPIO.PWM(VERTICAL_GPIO, 50)
-  h.start(5.4) # 1.8 à 9.2
-  v.start(5) # 4.6 à 7
+  pied.init()
   # Bluetooth
   bt.connect()
   # passage à l'étape suivante
