5db89d86e4a95c43224add6f15b4c36276ac475c
pessonnier <loic.pessonnier@gmail.com>
2017-07-01 14:43:26 +0200

    Update identification.py

--- identification.py
+++ identification.py
@@ -8,10 +8,10 @@ import os
 def initCamera():
   print('camera')
   camera = picamera.PiCamera()
-  camera.resolution = (320, 240)
+  camera.resolution = (conf.CAMERA_CAPTURE_X,conf.CAMERA_CAPTURE_Y)
   camera.start_preview()
-  output = np.empty((240, 320, 3), dtype=np.uint8)
-  ima = np.zeros((240,320,3), dtype=np.uint8)
+  output = np.empty((conf.CAMERA_CAPTURE_Y,conf.CAMERA_CAPTURE_X, 3), dtype=np.uint8)
+  ima = np.zeros((conf.CAMERA_CAPTURE_Y,conf.CAMERA_CAPTURE_X,3), dtype=np.uint8)
   over = camera.add_overlay(ima.tobytes(), layer=3, alpha=64)
   return camera, output, ima, over
 
@@ -25,6 +25,38 @@ def initReco():
   visagesCodes = [ face_recognition.face_encodings(v)[0] for v in visages ]
   return visagesCodes
 
+def precapture():
+  camera, output, ima, over = initCamera()
+  visagesCodes = initReco()
+  face_locations = lambda :
+    camera.capture(output, format="rgb")
+    return face_recognition.face_locations(output)
+  face_encodings = lambda loc:
+    return face_recognition.face_encodings(output, loc)
+  face_le = lambda :
+    loc = face_locations()
+    return face_recognition.face_encodings(output, loc), loc
+  compare_faces = lambda face_encodings:
+    matchs = [ False ] * len(visagesCodes)
+    for face_encoding in face_encodings:
+      #agrege tous les fichiers identifiés dans matchs
+      matchs = [ matchs[i] | m for i, m in enumerate(face_recognition.compare_faces(visagesCodes, face_encoding)) ]
+    imatchs = [ i for i, m in enumerate(matchs) if m]
+    return match, imatch
+  face_lec = lambda :
+    return compare_faces (face_le ()[0])[1]
+  return face_lec, camera, output, ima, over, visagesCodes, face_locations, face_encodings, compare_faces
+
+def dessineCadre(over):
+  #ajout des cadres
+  ima = np.zeros((conf.CAMERA_CAPTURE_Y,conf.CAMERA_CAPTURE_X,3), dtype=np.uint8)
+  for i,(top, right, bottom, left) in enumerate(face_locations):
+    ima[top, left:right, :] = 0xff
+    ima[bottom, left:right, :] = 0xff
+    ima[top:bottom, left, :] = 0xff
+    ima[top:bottom, right, :] = 0xff
+  over.update(ima.tobytes())
+
 def rechercheVisage(camera, output, ima, over, visagesCodes):
   print('recherche')
   face_locations = []
@@ -35,9 +67,8 @@ def rechercheVisage(camera, output, ima, over, visagesCodes):
     #print("Found {} faces in image.".format(len(face_locations)))
 
     #ajout des cadres
-    ima = np.zeros((240,320,3), dtype=np.uint8)
+    ima = np.zeros((conf.CAMERA_CAPTURE_Y,conf.CAMERA_CAPTURE_X,3), dtype=np.uint8)
     for i,(top, right, bottom, left) in enumerate(face_locations):
-      #print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
       ima[top, left:right, :] = 0xff
       ima[bottom, left:right, :] = 0xff
       ima[top:bottom, left, :] = 0xff
@@ -69,3 +100,11 @@ def test():
   camera, output, ima, over = initCamera()
   visagesCodes = initReco()
   rechercheVisage(camera, output, ima, over, visagesCodes) 
+
+def test2():
+  f=precapture()
+  for i in range(10):
+    print(f[0]())
+  
+  
+  
